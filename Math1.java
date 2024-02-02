6cd5ae26c16687e455eb76e8b3b7af3fb6e5a996
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-02-02 15:52:01 +0300

    Java Math

---
+++ Math1.java
@@ -0,0 +1,16 @@
+public class Math1 {
+
+  //math in java
+  public static void main(String[] args) {
+    // Math.max(x, y)
+    System.out.println(Math.max(5, 10));
+    // Math.min(x, y)
+    System.out.println(Math.min(5, 10));
+    // Math.sqrt(x)
+    System.out.println(Math.sqrt(64));
+    // Math.abs(x)
+    System.out.println(Math.abs(-4.7));
+    // Math.random()
+    System.out.println(Math.random());
+  }
+}
