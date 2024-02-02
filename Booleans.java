6ebc81ae4d6c75d00c70d2358165cbb03f0c86f0
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-02-02 15:55:21 +0300

    Java Booleans

---
+++ Booleans.java
@@ -0,0 +1,9 @@
+public class Booleans {
+    //Booleans in java 
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+    }
+}
