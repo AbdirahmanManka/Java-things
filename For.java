00e042b24237a1410422881af8b766151c4b8b07
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-02-02 19:14:04 +0300

    Cases and for loop in java

---
+++ For.java
@@ -0,0 +1,8 @@
+public class    For {
+    // java for loop
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ IF.java
@@ -0,0 +1,11 @@
+public class IF {
+    // java if else statements
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
+}
